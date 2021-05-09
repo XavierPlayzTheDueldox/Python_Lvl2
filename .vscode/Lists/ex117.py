f190d2bd59e154fc9e8baf77490efddbdc632fba
XavierPlayzTheDueldox <xavier10.xo@gmail.com>
2021-05-09 16:22:19 +0800

    Initial Commit

--- .vscode/Lists/ex117.py
+++ .vscode/Lists/ex117.py
@@ -1,17 +1,39 @@
-def line_of_best_fit(linelist):
-    b = linelist[1]
-    rise = linelist[-1] - linelist[1]
-    step = linelist[-2]
-    m = rise / step
-    return f'y = {m}x +{b}'
+def line_of_best_fit(x,y):
+    sx = sum(x)
+    sy = sum(y)
+    sxy = 0
 
-def readval():
-    numberslist = list()
-    ans=''
-    while not ans=='':
-        ans = input('Enter a number. Enter blank to proceed.')
-        if ans != '':
-            numberslist.insert(len(numberslist),int(ans))
-    return numberslist
+    sxsq = 0
+    for i in range(len(x)):
+        sxsq += x[i] ** 2
+    sxsq2 = sum(x) ** 2
+    n = len(x)
 
-print(line_of_best_fit(readval())
+    for i in range(len(x)):
+        sxy += x[0] * y[0]
+        x.pop(0)
+        y.pop(0)
+
+    m = (sxy - ((sx*sy)/n))
+    m /= (sxsq - (sxsq2/n))
+
+    b = (sy/n) - (m * (sx/n))
+
+    return f'y = {m:.2f}x +{b:.1f}'
+
+
+x = list()
+y = list()
+ans=1
+while not ans=='':
+    ans = input('Enter a number. Enter blank to proceed.')
+    if ans != '':
+        x.insert(len(x),float(ans))
+    if ans == '':
+        break
+    ans = input('Enter a number. Enter blank to proceed.')
+    if ans != '': 
+       y.insert(len(y),float(ans))
+
+
+print(line_of_best_fit(x, y))
